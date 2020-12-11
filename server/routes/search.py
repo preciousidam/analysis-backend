38653b8e2c320e54c3a5518739333bf337c2be83
Precious Idam <pidam@keystonebankng.com>
2020-12-11 15:11:17 +0100

    implement search property by name, area, state, facilities

--- server/routes/search.py
+++ server/routes/search.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
 from operator import add
 import numpy
-from sqlalchemy import func
+from sqlalchemy import func, or_
 from flask_cors import CORS
 
 from server.models.Properties import Property, Price
@@ -18,16 +18,10 @@ CORS(searchRoute)
 def compare():
     keyword = request.args.get('keyword')
     
-    prop = Property.query.filter_by(
-                                    area=keyword, 
-                                    bedrooms=keyword, 
-                                    type=keyword,
-                                    name=keyword,
-                                    facilities=keyword,
-                                    address=keyword,
-                                    serv_charge=keyword,
-                                    sale_price=keyword
-                                ).order_by(func.random()).first()
+    prop = Property.query.filter(or_(Property.area.ilike(f'%{keyword}%'),
+        Property.state.ilike(f'%{keyword}%'), Property.bedrooms.ilike(f'%{keyword}%'),
+        Property.facilities.ilike(f'%{keyword}%'),Property.name.ilike(f'%{keyword}%'),
+    )).order_by(func.random()).all()
     
     return jsonify({'property': prop, 'msg': 'success'}), 200
 
