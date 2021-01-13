4c09ce220c3773b5ee901f886618c3c90bb4917e
preciousidam <preciousidam@gmail.com>
2021-01-13 09:12:12 +0100

    removed unused routes

--- server/routes/statistics.py
+++ server/routes/statistics.py
@@ -38,36 +38,7 @@ def avarage_rent(bed):
     return jsonify({'data': allAverage, 'msg': 'success'}), 200
 
 
-@statRoute.route('/compare')
-def compare():
-    bed = request.args.get('bed')
-    area = request.args.get('area')
-    typeOf = request.args.get('type')
-    comarea = request.args.get('comarea')
-    props = Property.query.filter_by(area=area, bedrooms=bed, type=typeOf).all()
-    propCom = Property.query.filter_by(area=comarea, bedrooms=bed, type=typeOf).all()
-    stats = {area: [0,0,0,0,0], comarea: [0,0,0,0,0]}
-    
-    for prop in props:
-        price = list(map(prices,prop.rents))
-        price = numpy.pad(price, pad_width=(5-len(price),0))
-        stats[area] = list(map(add, stats[area],price))
-    
-    for prop in propCom:
-        price = list(map(prices,prop.rents))
-        price = numpy.pad(price, pad_width=(5-len(price),0))
-        stats[comarea] = list(map(add,stats[comarea],price))
 
-    count1 = len(props)
-    count2 = len(propCom)
-
-
-    if count1 > 0:
-        stats[area] = list(numpy.floor_divide(stats[area],count1))
-    if count2 > 0:
-        stats[comarea] = list(numpy.floor_divide(stats[comarea],count2))
-    
-    return jsonify({'data': stats, 'msg': 'success'}), 200
 
 
 @statRoute.route('/<path:area>')
