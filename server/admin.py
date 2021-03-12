9e0ced5f04985789237c7d38993c27a928b7dd07
preciousidam <preciousidam@gmail.com>
2021-03-12 20:43:49 +0100

    added abuja and areas in abuja

--- server/admin.py
+++ server/admin.py
@@ -127,8 +127,10 @@ class UserAdminView(MyModelView):
             model.password = generate_password_hash(model.password)
 
 class PropertyAdmin(MyModelView):
-    form_choices = {'area': [('ikoyi', 'Ikoyi'), ('vi', 'Victoria Island'), ('lekki', 'Lekki'), ('oniru', 'Oniru'), ('ph', 'Port Harcout')],
-                    'state': [('lagos', 'Lagos'),  ('port-harcourt', 'Port Harcout')],
+    form_choices = {'area': [('ikoyi', 'Ikoyi'), ('vi', 'Victoria Island'), ('lekki', 'Lekki'), ('oniru', 'Oniru'), ('ph', 'Port Harcout'),
+                                ('maitama', 'Maitama'), ('asokoro', 'Asokoro'), ('wuse II', 'Wuse II'), 
+                                ('mabushi', 'Mabushi'), ('jabi', 'Jabi'), ('utako', 'Utako')],
+                    'state': [('abuja', 'Abuja'), ('lagos', 'Lagos'), ('port-harcourt', 'Port Harcout')],
                     'bedrooms': [(1,'1 Bedroom'), (2, '2 Bedroom'), (3, '3 Bedroom'), (4, '4 Bedroom'), (5, '5 Bedroom'), (6, '6 Bedroom'), (7, '7 Bedroom')],
                     'type': [('Flat','Flat'), ('pent house', 'Pent House'), ('terrace', 'Terrace'), ("duplex", 'Duplex'), ("maisonette", 'Maisonette')]
                     }
