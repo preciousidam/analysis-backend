e47b6c53aec807011539d99261be4c181a26dc20
Ebubechukwu Idam <68309549+preciousidam@users.noreply.github.com>
2022-08-08 10:08:16 +0100

    statz

--- server/routes/statistics.py
+++ server/routes/statistics.py
@@ -1,40 +1,38 @@
 from flask import Blueprint, jsonify, request
 from flask_cors import CORS
 from functools import reduce
+from sqlalchemy import func, or_
 
 from server.models.Properties import Property, Price
 from server.util.instances import db
-from server.util.helpers import (no_of_Beds, get_areas, 
-    get_years, get_types, get_average, get_average_type)
+from server.util.helpers import (no_of_Beds, get_areas,
+                                 get_years, get_types, get_average, get_average_type)
 
 statRoute = Blueprint('statistics', __name__, url_prefix="/api/stats")
 
 
 CORS(statRoute)
 
-def prices (a):
+
+def prices(a):
     return a.amount
 
 
 @statRoute.route('/all-average/<int:bed>', methods=['GET'])
 def avarage_rent(bed):
-    areas = ['vi','ikoyi','wuse II', 'ph', 'lekki', 'maitama']
+    commercial_type = request.args.get('commercial_type') == 'true'
+    areas = ['vi', 'ikoyi', 'wuse II', 'ph', 'lekki', 'maitama']
     allAverage = dict(ikoyi=[], vi=[])
     years = get_years()[-3:]
-    print(years)
-    
+
     for area in areas:
         yearAve = []
         for year in years:
-            yearAve.append(get_average(year, area, bed))
-        
+            yearAve.append(get_average(year, area, bed, commercial_type))
+
         allAverage.update({area: yearAve})
 
-
     return jsonify({'data': allAverage, 'msg': 'success'}), 200
-
-
-
 
 
 @statRoute.route('/<path:area>')
@@ -42,15 +40,15 @@ def area_stats(area):
     beds = no_of_Beds()
     allAverage = dict()
     years = get_years()
-    aptType = request.args.get('type','flat')
-    
+    aptType = request.args.get('type', 'flat')
+    is_comm = request.args.get('commercial', '')
+
     for bed in beds:
         yearAve = []
         for year in years:
-            yearAve.append(get_average_type(year, area, bed, aptType))
-        
-        allAverage.update({bed: yearAve})
+            yearAve.append(get_average_type(year, area, bed, aptType, is_comm))
 
+        allAverage.update({bed: yearAve})
 
     return jsonify({'data': allAverage, 'msg': 'success'}), 200
 
@@ -59,26 +57,36 @@ def area_stats(area):
 def min_max(area):
     beds = no_of_Beds()
     year = get_years()[-1]
-    type = request.args.get('type','flat')
+    type = request.args.get('type', 'flat')
     result = dict()
 
     for bed in beds:
-        prices = Price.query.filter_by(year=year).join(Price.property).filter(Property.bedrooms==bed, Property.type == type, Property.area == area).all()
+        prices = Price.query.filter_by(year=year).join(Price.property).filter(
+            Property.bedrooms == bed, Property.type == type, Property.area == area).all()
         if(len(prices) > 0):
-            max = reduce(lambda acc, next: acc if acc >= next else next, prices)
-            min = reduce(lambda acc, next: acc if acc <= next else next, prices)
+            max = reduce(lambda acc, next: acc if acc >=
+                         next else next, prices)
+            min = reduce(lambda acc, next: acc if acc <=
+                         next else next, prices)
 
-            result[bed] = dict(max=max.property,min=min.property)
-
-    
+            result[bed] = dict(max=max.property, min=min.property)
 
     return jsonify({'data': result, 'msg': 'success'}), 200
 
+
 @statRoute.route('/types/<path:area>')
 def type_stat(area):
-    
+    is_comm = request.args.get('commercial')
+    query = Property.query
+
+    if is_comm == 'true':
+        query = query.filter(Property.is_commercial == True)
+    elif is_comm == 'false':
+        query = query.filter(or_(Property.is_commercial ==
+                             False, Property.is_commercial == None))
+
     total = dict()
-    properties = Property.query.filter_by(area=area.lower()).all()
+    properties = query.filter_by(area=area.lower()).all()
 
     for prop in properties:
         if prop.type in total:
--- server/util/helpers.py
+++ server/util/helpers.py
@@ -6,6 +6,7 @@ import numpy as np
 from server.models.Properties import Property, Price
 from server.util.instances import db
 
+
 class AreaSet:
     def __init__(self) -> None:
         self.items = list()
@@ -41,6 +42,7 @@ def get_areas():
 
     return dst.all()
 
+
 def get_years():
     properties = Property.query.all()
     dst = set()
@@ -50,6 +52,7 @@ def get_years():
             dst.add(rent.year)
 
     return list(dst)
+
 
 def get_types():
     properties = Property.query.all()
@@ -61,35 +64,46 @@ def get_types():
     return list(dst)
 
 
-def findAll(q,type, page):
-    per_page=10
+def findAll(q, type, page):
+    per_page = 10
     prop = Property.query.filter_by(type=type)\
-        .filter(or_(Property.area.ilike(f'%{q}%'),Property.name.ilike(f'%{q}%')))
+        .filter(or_(Property.area.ilike(f'%{q}%'), Property.name.ilike(f'%{q}%')))
 
-    total = prop.count()     
-    
-    return prop.paginate(page,per_page,error_out=False).items, total
+    total = prop.count()
+
+    return prop.paginate(page, per_page, error_out=False).items, total
 
 
-def get_average_type(year, area, bed, type):
+def get_average_type(year, area, bed, type, is_comm):
+    prices = None
+    priceQuery = Price.query.filter_by(year=year).join(Price.property)
+    if is_comm == 'true':
+        prices = np.array(priceQuery.filter(
+            Property.bedrooms == bed, Property.type == type, Property.area == area, Property.is_commercial == True).all())
+    elif is_comm == 'False':
+        prices = np.array(priceQuery.filter(
+            Property.bedrooms == bed, Property.type == type, Property.area == area, or_(Property.is_commercial == False, Property.is_commercial == None)).all())
+    else:
+        prices = np.array(priceQuery.filter(
+            Property.bedrooms == bed, Property.type == type, Property.area == area).all())
 
-    prices = np.array(Price.query.filter_by(year=year).join(Price.property).filter(Property.bedrooms==bed, Property.type == type, Property.area == area).all())
-    
     sumTotal = reduce(lambda acc, a: acc + a.amount, prices, 0)
-    
+
     '''if np.size(prices) > 0:
         return sumTotal/np.size(prices)'''
     return sumTotal/(np.size(prices) if np.size(prices) else 1)
 
 
-def get_average(year, area, bed):
+def get_average(year, area, bed, comm_type):
+    values = Price.query.filter_by(year=year).join(Price.property)
+    if comm_type == 'commercial':
+        values = values.filter(Property.is_commercial == True)
+    elif comm_type == 'non-commercial':
+        values = values.filter(
+            or_(Property.is_commercial == False, Property.is_commercial == None))
+    prices = np.array(values.filter(Property.bedrooms ==
+                      bed, Property.area == area).all())
 
-    prices = np.array(Price.query.filter_by(year=year).join(Price.property).filter(Property.bedrooms==bed, Property.area == area).all())
-    
     sumTotal = reduce(lambda acc, a: acc + a.amount, prices, 0)
-    
 
     return sumTotal/(np.size(prices) if np.size(prices) else 1)
-
-    
-    
